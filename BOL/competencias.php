52bdef5513570a744a496e79751babefab6f04bb
dcusipuma29 <44351308+dcusipuma29@users.noreply.github.com>
2018-10-27 09:10:51 -0500

    DayanaC
    BOL - COMPETENCIAS
    DAO - COMPETENCIASDAO
    DESIGNER - FRMCOMPETENCIAS

---
+++ BOL/competencias.php
@@ -0,0 +1,17 @@
+<?php
+class Competencias
+{
+	private $nombreCompetencia;
+	private $numeroCo;
+
+
+	public function __GET($x)
+	{ 
+		return $this->$x; 
+	}
+	public function __SET($x, $y)
+	{ 
+		return $this->$x = $y; 
+	}
+}
+?>

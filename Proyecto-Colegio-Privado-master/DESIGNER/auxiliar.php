7d1ec69e46d84b769f2e10d66d697ebb64c1c357
AlexandreTMz <jimmy.97.amor@gmail.com>
2018-10-27 19:31:26 -0500

    SOLO LOS ARCHIVOS DAO Y BOL
    Porfavor solo los archivos DAO Y BOL NO TODO LA CARPETA _!!!!

--- Proyecto-Colegio-Privado-master/DESIGNER/auxiliar.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION["id"] = $_GET['id'];
-header('Location: frmCapacidad.php');
-
-?>
--- Proyecto-Colegio-Privado-master/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-?>
-<html>
-<a href="DESIGNER/frmPersona.php">Persona</a>
-<a href="DESIGNER/frmCompetencias.php">Persona</a>
-</html>

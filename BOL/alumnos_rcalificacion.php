ff8dcce47c190e6933caf1d490782c240d65f998
jgutierrez1602 <jorgeguti1602@gmail.com>
2018-10-28 21:29:48 -0500

    avancegutierrez
    tablas alumnos_rcalificacion , grados_curso

--- BOL/alumnos_rcalificacion.php
+++ BOL/alumnos_rcalificacion.php
@@ -1,20 +1,23 @@
 <?php
 class Alumnos_rcalificacion
 {
-    private $id_arcalificacion;
-	private $id_estudiante;
+  private $id_arcalificacion;
 	private $id_rcalificacion;
+  private $id_estudiante;
 	private $nota_final;
 
-
+  public function _CONSTRUCT(){
+		$this-> id_arcalificacion = new Arcalificacion_notas();
+		$this-> id_rcalificacion = new Registro_calificacion();
+		$this-> id_estudiante = new Estudiante();
 
 	public function __GET($x)
-	{ 
-		return $this->$x; 
+	{
+		return $this->$x;
 	}
 	public function __SET($x, $y)
-	{ 
-		return $this->$x = $y; 
+	{
+		return $this->$x = $y;
 	}
 }
 ?>
